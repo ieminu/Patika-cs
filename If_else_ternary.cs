464b969e70571e39c68bb22fb3408b6c89fb33ed
ieminu <98277683+ieminu@users.noreply.github.com>
2022-02-13 14:55:13 +0300

    Add files via upload

---
+++ If_else_ternary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+ namespace Namespace_name 
+ {
+     class MainClass
+     {
+        static void Main(string[] args)
+        {
+            int saat = DateTime.Now.Hour;
+
+            if (saat >= 6 && saat <= 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+
+            else if (saat <= 18)
+            {
+                Console.WriteLine("İyi Günler!");
+            }
+
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+
+            string mesaj = saat >= 6 && saat <= 11 ? "Günaydın!" : saat <= 18 ? "İyi Günler!" : "İyi Geceler!";
+
+            Console.WriteLine(mesaj);
+        }
+     }
+ }
