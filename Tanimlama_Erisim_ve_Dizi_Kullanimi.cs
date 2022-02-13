2b1c01b9307d4a90ea1314951dfb1a6a5ec3127a
ieminu <98277683+ieminu@users.noreply.github.com>
2022-02-13 15:00:08 +0300

    Add files via upload

---
+++ Tanimlama_Erisim_ve_Dizi_Kullanimi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+ namespace Namespace_name 
+ {
+     class MainClass
+     {
+        static void Main(string[] args)
+        {
+            //Dizi tanımlama yöntemleri :
+
+            int[] AsalSayilar;
+            AsalSayilar = new int[4];
+            AsalSayilar[2] = 5;
+
+            string[] Renkler = new string[4];
+            Renkler[1] = "mavi";
+
+            string[] Hayvanlar = {"kedi", "köpek", "balık", "sincap"};
+
+            Console.WriteLine(AsalSayilar[2] + " " + Renkler[1] + " " + Hayvanlar[0]);
+
+
+            //Döngülerle Dizi Kullanımı :
+
+            Console.Write("Dizinin eleman sayısını giriniz: ");
+            int DiziUzunlugu = int.Parse(Console.ReadLine());
+            int[] Sayilar = new int[DiziUzunlugu];
+
+            for (int i = 1; i <= DiziUzunlugu; i++)
+            {
+                Console.WriteLine("{0}. sayıyı giriniz: " , i);
+                Sayilar[i-1] = int.Parse(Console.ReadLine());
+            }
+
+            float Toplam = 0;
+
+            foreach (var Sayi in Sayilar)
+                Toplam += Sayi;
+
+            Console.WriteLine("Ortalama: " + Toplam/DiziUzunlugu);
+        }
+     }
+ }
